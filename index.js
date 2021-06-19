14777074a40377d5f216db1666cd47c719fcd0ac
Usman Babakolo <usmansbk@gmail.com>
2021-06-19 21:16:01 +0100

    Elevate header on scroll

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const openMenuBtn = document.querySelector('#open-menu');
 const closeMenuBtn = document.querySelector('#close-menu');
 const mobileNav = document.querySelector('#mobile-nav');
+const appBar = document.querySelector('.app-bar');
 const menu = document.querySelector('.mobile-menu');
 const overlay = Array.from(document.querySelectorAll('header, .headline, .works, .about-container, .contact-me'));
 
@@ -16,6 +17,24 @@ function onClickNav() {
   toggleMenu();
 }
 
+let scrolling = false;
+function onScroll() {
+  scrolling = true;
+}
+
+setInterval(() => {
+  if (scrolling) {
+    scrolling = false;
+    const scrollY = Math.round(window.scrollY);
+    if (scrollY) {
+      appBar.classList.add('elevate-header');
+    } else {
+      appBar.classList.remove('elevate-header');
+    }
+  }
+}, 300);
+
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
 mobileNav.addEventListener('click', onClickNav);
+window.addEventListener('scroll', onScroll, { passive: true });
