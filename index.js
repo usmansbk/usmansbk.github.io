c30216b55d2ea038bc1ca06ce6beda52b4db1004
Usman Babakolo <usmansbk@gmail.com>
2021-06-19 14:35:59 +0100

    Hide menu on click nav item

--- index.js
+++ index.js
@@ -2,44 +2,22 @@ const openMenuBtn = document.querySelector('#open-menu');
 const closeMenuBtn = document.querySelector('#close-menu');
 const mobileNav = document.querySelector('#mobile-nav');
 const menu = document.querySelector('.mobile-menu');
-const menuOverlay = document.querySelectorAll('.menu-overlay');
+const overlay = Array.from(document.querySelectorAll('header, .headline, .works, .about-container, .contact-me'));
 
-function openMenu(event) {
-  event.preventDefault();
-  /**
-   * TODO
-   * Set "menu" style
-   * .mobile-menu { display: block }
-   *
-   * Set the style of each "menuOverlay" node
-   * .menu-overlay {
-   *    filter: blur(8px);
-   * }
-   */
-}
-
-function hideMenu() {
-  /**
-   * TODO
-   * Set "menu" style
-   * .mobile-menu { display: none; }
-   *
-   * Set the style of each "menuOverlay" node
-   * .menu-overlay {
-   *    filter: none;
-   * }
-   */
+function toggleMenu(event) {
+  if (event) {
+    event.preventDefault();
+  }
+  menu.classList.toggle('open-menu');
+  for (let part of overlay) {
+    part.classList.toggle('menu-overlay')
+  }
 }
 
 function onClickNav() {
-  hideMenu();
+  toggleMenu();
 }
 
-function closeMenu(event) {
-  event.preventDefault();
-  hideMenu();
-}
-
-openMenuBtn.addEventListener('click', openMenu);
-closeMenuBtn.addEventListener('click', closeMenu);
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
 mobileNav.addEventListener('click', onClickNav);
