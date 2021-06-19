a0ca34611426f77524a75a264dcf0c3d9a8d3f16
Usman Babakolo <usmansbk@gmail.com>
2021-06-19 13:43:42 +0100

    Fix query selector

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const openMenuBtn = document.querySelector('#open-menu');
 const closeMenuBtn = document.querySelector('#close-menu');
+const mobileNav = document.querySelector('#mobile-nav');
 const menu = document.querySelector('.mobile-menu');
-const mobileNav = document.querySelector('.mobile-nav');
 const menuOverlay = document.querySelectorAll('.menu-overlay');
 
 function openMenu(event) {
