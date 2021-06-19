8fd6c6f890d957682a9e9431789bb1c69343e035
Usman Babakolo <usmansbk@gmail.com>
2021-06-19 13:42:15 +0100

    Fix query

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const openMenuBtn = document.querySelector('#open-menu');
 const closeMenuBtn = document.querySelector('#close-menu');
 const menu = document.querySelector('.mobile-menu');
-const mobileNav = document.querySelectorAll('.mobile-nav');
+const mobileNav = document.querySelector('.mobile-nav');
 const menuOverlay = document.querySelectorAll('.menu-overlay');
 
 function openMenu(event) {
