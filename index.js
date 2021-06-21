84229c1fe7870cffa514bb3b9db1ff4c04c50a19
Usman Babakolo <usmansbk@gmail.com>
2021-06-21 19:29:42 +0100

    Add projects data

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const DATA = {
+  Tonic: {
+    title: 'Tonic',
+    image: 'images/snapshots/tonic.svg',
+    captions: ['CANOPY', 'Back End Dev', '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tags: ['html', 'css', 'javaScript'],
+  },
+  'Multi-Post Stories': {
+    title: 'Multi-Post Stories',
+    image: 'images/snapshots/stories.svg',
+    captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  },
+  'Facebook 360': {
+    title: 'Facebook 360',
+    image: 'images/snapshots/facebook.svg',
+    captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  },
+  'Uber Navigation': {
+    title: 'Uber Navigation',
+    image: 'images/snapshots/uber.svg',
+    captions: ['Uber', 'Lead Developer', '2018'],
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  },
+};
+
 const openMenuBtn = document.getElementById('open-menu');
 const closeMenuBtn = document.getElementById('close-menu');
 const navLinks = document.getElementById('mobile-nav');
