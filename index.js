9d252b0a992577f9831f447ac961d5984b51592e
Usman Babakolo <usmansbk@gmail.com>
2021-06-22 05:46:12 +0100

    Add live and source links

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const DATA = {
     captions: ['CANOPY', 'Back End Dev', '2015'],
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tags: ['html', 'css', 'javaScript'],
+    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+    liveLink: 'https://usmansbk.github.io/',
   },
   'Multi-Post Stories': {
     title: 'Multi-Post Stories',
@@ -12,6 +14,8 @@ const DATA = {
     captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+    liveLink: 'https://usmansbk.github.io/',
   },
   'Facebook 360': {
     title: 'Facebook 360',
@@ -19,6 +23,8 @@ const DATA = {
     captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
     description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+    liveLink: 'https://usmansbk.github.io/',
   },
   'Uber Navigation': {
     title: 'Uber Navigation',
@@ -26,6 +32,8 @@ const DATA = {
     captions: ['Uber', 'Lead Developer', '2018'],
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+    liveLink: 'https://usmansbk.github.io/',
   },
 };
 
