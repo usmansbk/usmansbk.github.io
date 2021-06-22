68d7a74ff4256dc083a7e715d91492267b33d811
Usman Babakolo <usmansbk@gmail.com>
2021-06-22 08:51:25 +0100

    Setup commit

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const DATA = [
-  {
-    title: 'Tonic',
-    image: 'images/snapshots/tonic.svg',
-    captions: ['CANOPY', 'Back End Dev', '2015'],
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    tags: ['html', 'css', 'javaScript'],
-    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
-    liveLink: 'https://usmansbk.github.io/',
-  },
-  {
-    title: 'Multi-Post Stories',
-    image: 'images/snapshots/stories.svg',
-    captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
-    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
-    liveLink: 'https://usmansbk.github.io/',
-  },
-  {
-    title: 'Facebook 360',
-    image: 'images/snapshots/facebook.svg',
-    captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
-    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
-    liveLink: 'https://usmansbk.github.io/',
-  },
-  {
-    title: 'Uber Navigation',
-    image: 'images/snapshots/uber.svg',
-    captions: ['Uber', 'Lead Developer', '2018'],
-    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
-    liveLink: 'https://usmansbk.github.io/',
-  },
-];
+// const DATA = [
+//   {
+//     title: 'Tonic',
+//     image: 'images/snapshots/tonic.svg',
+//     captions: ['CANOPY', 'Back End Dev', '2015'],
+//     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+//     tags: ['html', 'css', 'javaScript'],
+//     sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+//     liveLink: 'https://usmansbk.github.io/',
+//   },
+//   {
+//     title: 'Multi-Post Stories',
+//     image: 'images/snapshots/stories.svg',
+//     captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
+//     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+//     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+//     sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+//     liveLink: 'https://usmansbk.github.io/',
+//   },
+//   {
+//     title: 'Facebook 360',
+//     image: 'images/snapshots/facebook.svg',
+//     captions: ['FACEBOOK', 'Full Stack Dev', '2015'],
+//     description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+//     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+//     sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+//     liveLink: 'https://usmansbk.github.io/',
+//   },
+//   {
+//     title: 'Uber Navigation',
+//     image: 'images/snapshots/uber.svg',
+//     captions: ['Uber', 'Lead Developer', '2018'],
+//     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+//     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+//     sourceCode: 'https://github.com/usmansbk/usmansbk.github.io',
+//     liveLink: 'https://usmansbk.github.io/',
+//   },
+// ];
 
 const openMenuBtn = document.getElementById('open-menu');
 const closeMenuBtn = document.getElementById('close-menu');
@@ -294,7 +294,7 @@ function loadProjects(data = []) {
   });
 }
 
-loadProjects(DATA);
+// loadProjects(DATA);
 
 function toggleMenu() {
   menu.classList.toggle('open-menu');
